89b19cd134ff26e7b7fc7b423fa4d659aa1fa46b
Michael Roth <>
2017-05-13 03:55:31 +0200

    major Encrypter update: * encrypter now passes non-letters (such as spaces) unencrypted * using charType to determine type of Character instead of its value - deleted deprecated ArrayList using decrypt methods

--- src/Vigener/Encrypter.java
+++ src/Vigener/Encrypter.java
@@ -8,64 +8,19 @@ import java.util.ArrayList;
  * in further versions there (hopefully) will be method that allows the keying of the key table as well
  *@since 2.0
  *@author Michael Roth
+ *@version 3.0
  */
 public final class Encrypter {
-    /*public static String encrypt(String msg, String messageKey) {
-        
-        // converts the input Strings to acceptable form
-        String tableKey = KeyTable.process(messageKey, false); 
-        
-        
-        StringBuilder sB = new StringBuilder(); // creates the StringBuilder to better manipulate the Strings
-        // StringBuilder sTestB = new StringBuilder(); // just to test
-        
-        ArrayList <Character> arr = KeyTable.getTableKey(true); // actually the only way to 
-        
-        int j = 0;
-        for(int i = 0; i < msg.length(); i++) { // need to use iterator, multiple variables rely on it
-            // sB.append(KeyTable.shift(arr,arr.indexOf(tableKey.charAt(i % tableKey.length()))).get(arr.indexOf(msg.charAt(i)))); // turns out to throw Dory, at least I know where...
-            if(msg.charAt(i) > 'z' || (msg.charAt(i) < 'a' && msg.charAt(i) > 'Z') || msg.charAt(i) < 'A') {
-                sB.append(msg.charAt(i)); 
-            } else {
-                boolean upperCase = Character.isUpperCase(msg.charAt(i));
-                ArrayList <Character> ar = new ArrayList <> (KeyTable.shift(KeyTable.getTableKey(upperCase),arr.indexOf(tableKey.charAt(j%tableKey.length())))); // was throwing Dory, don't know why, it does'n do it anymore
-               sB.append(ar.get(KeyTable.getTableKey(upperCase).indexOf(msg.charAt(i)))); // actually works, DO NOT TOUCH
-                j++;
-            } // end if()
-        
-        } // end for()
-        return sB.toString();
-    } // end static method encrypt(String, String)
-    public static String encrypt(String msg, String messageKey, String tableKey) {
-        
-        // converts the input Strings to acceptable form
-        String tableKey = KeyTable.process(messageKey, false); 
-        
-        
-        StringBuilder sB = new StringBuilder(); // creates the StringBuilder to better manipulate the Strings
-        // StringBuilder sTestB = new StringBuilder(); // just to test
-        
-        ArrayList <Character> arr = KeyTable.getTableKey(tableKey, true); // actually the only way to 
-        
-        int j = 0;
-        for(int i = 0; i < msg.length(); i++) { // need to use iterator, multiple variables rely on it
-            // sB.append(KeyTable.shift(arr,arr.indexOf(tableKey.charAt(i % tableKey.length()))).get(arr.indexOf(msg.charAt(i)))); // turns out to throw Dory, at least I know where...
-            if(msg.charAt(i) > 'z' || (msg.charAt(i) < 'a' && msg.charAt(i) > 'Z') || msg.charAt(i) < 'A') {
-                sB.append(msg.charAt(i)); 
-            } else {
-                boolean upperCase = Character.isUpperCase(msg.charAt(i));
-                ArrayList <Character> ar = new ArrayList <> (KeyTable.shift(KeyTable.getTableKey(tableKey, upperCase),arr.indexOf(tableKey.charAt(j%tableKey.length())))); // was throwing Dory, don't know why, it does'n do it anymore
-                sB.append(ar.get(KeyTable.getTableKey(tableKey, upperCase).indexOf(msg.charAt(i)))); // actually works, DO NOT TOUCH
-                j++;
-            } // end if()
-        
-        } // end for()
-        return sB.toString();
-    } // end static method encrypt(String, String)
-    */
+    
     public static char encrypt(char input, String tableKey, char index) {
         
+        
         tableKey = KeyTable.process(tableKey,false,true);
+        
+        byte charType = (byte) Character.getType(input);
+        
+        if(charType != Character.LOWERCASE_LETTER && charType != Character.UPPERCASE_LETTER)
+            return input;
         
         int i = tableKey.indexOf(Character.toString(index).toUpperCase());
         
@@ -73,8 +28,10 @@ public final class Encrypter {
         
         int encryptedIndex = (i+j)%tableKey.length();
                 
-        if(input >= 'a' && input <= 'z') return tableKey.toLowerCase().charAt(encryptedIndex);
-        else return tableKey.toUpperCase().charAt(encryptedIndex);
+        if(charType == Character.LOWERCASE_LETTER) 
+            return tableKey.toLowerCase().charAt(encryptedIndex);
+        else 
+            return tableKey.toUpperCase().charAt(encryptedIndex);
     } // end static method encrypt(char, String, int)
     
     public static String encrypt(String input, String key, String tableKey) {
@@ -91,10 +48,9 @@ public final class Encrypter {
             
             byte charType = (byte) Character.getType(in); 
             
-            if(charType == Character.SPACE_SEPARATOR) b.append(in);
-            
-            if(charType == Character.LOWERCASE_LETTER || charType == Character.UPPERCASE_LETTER) {
-                
+            if(charType != Character.LOWERCASE_LETTER && charType != Character.UPPERCASE_LETTER) {
+                b.append(in);
+            } else {
                 char k = key.charAt(j%key.length());
                 
                 b.append(encrypt(in,tableKey,k));

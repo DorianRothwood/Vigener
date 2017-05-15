9e0e20067e8181768dc191425bdd10a6836d56c5
Michael Roth <micha@192.168.25.37>
2017-05-15 03:25:15 +0200

    minor patch + added javadoc for Decrypter and KeyTable classes * marked the ArrayList using KeyTable methods as deprecated, kept there for now * updated the KeyTable#getKey(String, boolean) method for use without ArrayList

--- src/Vigenere/Decrypter.java
+++ src/Vigenere/Decrypter.java
@@ -8,56 +8,100 @@ package Vigenere;
 import java.util.ArrayList;
 
 /**
- * this is work in progress (no progress made yet)
+ * this class is used for Vigenére cypher decryption.
+ * @see <a href = https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Vigen%C3%A8re_square_shading.svg/864px-Vigen%C3%A8re_square_shading.svg.png>Vigenére Square using default "ABCD...Z" table key</a>
  * @author Michael Roth
- * since 3.0
+ * @since 3.0
  */
 public final class Decrypter {
     
+    /**
+     * overload of method {@code decrypt(String,String,String)} for use with default table key
+     * @param emsg encrypted message of type {@code String}
+     * @param key encryption key of type {@code String}
+     * @return decrypted message of type {@code String}
+     * @see Vigenere.Decrypter#decrypt(java.lang.String, java.lang.String, java.lang.String) 
+     * @version 1.0
+     * @author Michael Roth
+     */
     public static String decrypt(String emsg ,String key) {
         return decrypt(emsg, key, KeyTable.getKey(true));
     } // end static method decrypt(String, String)
     
+    /**
+     * this method uses the {@link Vigenere.Decrypter#decrypt(char, char, java.lang.String)} method to decrypt the entire strings based on keyword
+     * @param emsg encrypted message of type {@code String}
+     * @param key encryption key of type {@code String}
+     * @param tableKey table key of type {@code String}
+     * @return original decrypted text of type {@code String}
+     * @see Vigenere.Decrypter#decrypt(char, char, java.lang.String) 
+     */
     public static String decrypt(String emsg, String key, String tableKey) {
         
+        // instantiates a new StringBuilder
         StringBuilder b = new StringBuilder();
         
+        // ensures key is in the right format (all upprecase, no non-letter characters
         key = KeyTable.process(key, false, true);
         
+        // declaration of chartype variable: will be used inside the cycle
         byte charType;
         
+        // iterator j, used inside the cycle to loop through the key
         int j = 0;
         
+        // iterator i is used to loop through the encrypted message
         for(int i = 0; i < emsg.length(); i++) {
             
+            // takes the character from the encrypted message
             char c = emsg.charAt(i);
             
+            // decides the type of given character
             charType = (byte) Character.getType(c);
             
+            // decides wether the current character should be decrypted(non-letter characters are left undecrypted)
             if(charType != Character.LOWERCASE_LETTER && charType != Character.UPPERCASE_LETTER) {
                 b.append(c);
             } else {
-                b.append(decrypt(c,key.charAt(j%key.length()),tableKey));
-                j++;
+                b.append(decrypt(c,key.charAt(j%key.length()),tableKey)); // calls the decrypt(char,String,char) method and stores the result in the StringBuilder
+                j++; // if the character is a letter, iterates the j iterator to loop through the key
             } // end if()
         } // end for()
         
+        // returns the String stored in this StringBuilder
         return b.toString();
     } // end method decrypt(String,String,String)
+    
+    /**
+     * method for decrypting one character, based on encrypted character and one of the coordinates on Vigenére Square
+     * @param e encrypted {@code char}
+     * @param k one of the known indexes of type {@code char}
+     * @param tableKey table key of type {@code String}
+     * @return second index of type {@code}
+     * @author Michael Roth
+     * @version 3.0
+     */
     public static char decrypt(char e, char k, String tableKey) {
         
+        // casts the character type to a byte variable (method returns int, whereas the constants in Character class are byte)
         byte charType = (byte) Character.getType(e);
         
+        // handles all non-letter characters by simply returning them (the cipher does not use them)
         if(charType != Character.LOWERCASE_LETTER && charType != Character.UPPERCASE_LETTER) return e;
         
+        // ensures the table key is correct format (uppercase and all letters used one time)
         tableKey = KeyTable.getKey(tableKey, true);
         
+        // stores the first coordinate of the encrypted letter
         int index = tableKey.indexOf(Character.toString(k).toUpperCase());
         
+        // shifts the key table String, so that the first coordinate has an index 0
         String shift = tableKey.substring(index).concat(tableKey.substring(0,index));
         
+        // index of the original letter is equal to the index of the encrypted letter in the shifted key table
         index = shift.indexOf(Character.toString(e).toUpperCase());
         
+        // based on the case of the encrypted letter, returns the original letter upper or lower case
         if(charType == Character.LOWERCASE_LETTER) 
             return Character.toLowerCase(tableKey.charAt(index));
         else
--- src/Vigenere/KeyTable.java
+++ src/Vigenere/KeyTable.java
@@ -10,12 +10,30 @@ import java.util.ArrayList;
  */
 public final class KeyTable {
     
+    /**
+     * default table key for general use with unencrypted tables
+     * @since 2.2
+     * @author Michael Roth
+     */
     private static final String DEFAULT_KEY = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
+    /**
+     * overload of method {@code getTableKey(boolean)} with upper case set by default
+     * @return table key of type {@code ArrayList <Character>}
+     * @deprecated use {@link Vigenere.KeyTable#getKey(boolean, java.lang.String) instead
+     */
+    @Deprecated
     public static ArrayList <Character> getTableKey() {
         return getTableKey(true);
     } // end method getTableKey()
     
+    /**
+     * this method returns an ArrayList with set of characters sorted into a table key
+     * @param upperCase {@code boolean} value to decide the case of characters inside the table
+     * @return table key of type {@code ArrayList <Character>}
+     * @deprecated use {@link Vigenere.KeyTable#getKey(boolean, java.lang.String) instead
+     */
+    @Deprecated
     public static ArrayList <Character> getTableKey(boolean upperCase)
     {
         String key;
@@ -25,12 +43,20 @@ public final class KeyTable {
         else key = DEFAULT_KEY.toLowerCase();
         
         for(int i = 0; i < key.length();i++) 
-            if((key.charAt(i) >= 'A' && key.charAt(i) <= 'Z') || (key.charAt(i) >= 'a' && key.charAt(i) <= 'z') && !(arr.contains(key.charAt(i)))) 
+            if((Character.getType(key.charAt(i)) == Character.UPPERCASE_LETTER) || (Character.getType(key.charAt(i)) == Character.LOWERCASE_LETTER) && !(arr.contains(key.charAt(i)))) 
                 arr.add(key.charAt(i));
         
         return arr;
     } // end method getTableKey(boolean)
     
+    /**
+     * this method returns an ArrayList with set of characters sorted into a table key using a non-standard key
+     * @param key table key of type {@code String} for more secure encryption
+     * @param upperCase {@code boolean} value to decide the case of characters inside the table
+     * @return table key of type {@code ArrayList <Character>}
+     * @deprecated use {@link Vigenere.KeyTable#getKey(boolean, java.lang.String) instead
+     */
+    @Deprecated
     public static ArrayList <Character> getTableKey(String key, boolean upperCase)
     {
         ArrayList <Character> arr = new ArrayList();
@@ -53,13 +79,14 @@ public final class KeyTable {
         return arr;
     } // end method getTableKey(String, boolean)
     /**
-     * this method has no good use yet, just to demonstrate how the Vigenere square works</br>
-     * eventually this method may be used in the GUI for encryption (Work not yet in progress)
+     * this method has no good use anymore, just to demonstrate how the Vigenere square works
      * @param arr {@code ArrayList} with the table key (keyed or unkeyed, supports both)
      * @return two-dimensional array of {@code char}s
      * @author Michael Roth
      * @since 2.0
+     * @deprecated not useful anymore
      */
+    @Deprecated
     public static char[][] getTable(ArrayList <Character> arr) {
         
         char[][] ar = new char[26][26];
@@ -67,18 +94,20 @@ public final class KeyTable {
         for(int i = 0; i < 26; i++) {
             for(int j = 0; j < 26; j++) {
                 ar[i][j] = arr.get(j);
-            }
+            } // end for
             shift(arr);
-        }
+        } // end for
         
         return ar;
     } // end static method getTable(ArrayList <Character>)
+    
     /**
      * this method shifts all the characters in the {@code ArrayList} by one to the left
      * @param arr {@code ArrayList} containing the table Key to be shifted
      * @return shifted {@code ArrayList}
      * @author Michael Roth
      * @since 2.0
+     * @deprecated not useful since it's not recommended to use ArrayList using KeyTable methods anymore
      */
     public static ArrayList <Character> shift(ArrayList <Character> arr) {
         char c = arr.get(0);
@@ -86,6 +115,7 @@ public final class KeyTable {
         arr.add(c);
         return arr;
     } // end method shift(ArrayList <Character>)
+    
     /**
      * this method overloads the original shift method with specified <b>n</b> for shift
      * @param arr {@code ArrayList} containing the table Key to be shifted
@@ -93,7 +123,9 @@ public final class KeyTable {
      * @return shifted {@code ArrayList}
      * @author Michael Roth
      * @since 2.2
+     * @deprecated not useful since it's not recommended to use ArrayList using KeyTable methods anymore
      */
+    @Deprecated
     public static ArrayList <Character> shift(ArrayList <Character> arr, int shiftCount) {
         for(int i = 0; i < shiftCount; i++) {
             char c = arr.get(0);
@@ -102,35 +134,54 @@ public final class KeyTable {
         } // end for
         return arr;
     } // end method shift(ArrayList <Character>, int)
+    
     /**
-     * this method takes the input {@code String}, sets it to upper case and removes all non-alphabetical characters
+     * this method is the overload for the method {@code process(String, boolean, boolean)} with upper case by default
      * since version 2.2 method also deletes the spaces (depending on the {@code <b>spaces</b> boolean} value) 
      * @param in input {@code String} to be processed
      * @param spaces {@code boolean} type to decide if processed string should contain spaces, usefull for processing both message and key
      * @return processed {@code String}
+     * @see Vigenere.KeyTable#process(java.lang.String, boolean, boolean) 
      * @author Michael Roth
      * @since 2.2
      */
     public static String process(String in,boolean spaces) {
-        StringBuilder s = new StringBuilder();
-        for(char c : in.toUpperCase().toCharArray()) {
-            if((c > 64 && c < 91) || (c == ' ' && spaces)) {
-                s.append(c);
-            } // end if()
-        } // end for()
-        return s.toString();
+        return process(in,spaces,true);
     } // end static method process(String, boolean)
+    
+    /**
+     * this method takes the input {@code String} and removes all non-alphabetical characters
+     * since version 2.2 method also deletes the spaces (depending on the {@code <b>spaces</b> boolean} value) 
+     * @param in input {@code String} to be processed
+     * @param spaces {@code boolean} type to decide if processed string should contain spaces, usefull for processing both message and key
+     * @param upperCase {@code boolean} to decide which case should the 
+     * @return processed {@code String}
+     * @author Michael Roth
+     * @since 2.2
+     */
     public static String process(String in,boolean spaces, boolean upperCase) {
         if(upperCase) return process(in,spaces);
         StringBuilder s = new StringBuilder();
         for(char c : in.toLowerCase().toCharArray()) {
-            if((c >= 'a' && c <= 'z') || (c == ' ' && spaces)) {
+            
+            // if a selected character is a letter or a blank space (if the spaces variable is true) the letter is appended into the StringBuilder
+            if((Character.getType(c) == Character.LOWERCASE_LETTER) || (c == ' ' && spaces)) {
                 s.append(c);
             } // end if()
         } // end for()
-        return s.toString();
+        
+        // based on the upperCase value returns either an uppercase or a lowercase String
+        if(upperCase) return s.toString().toUpperCase();
+        else return s.toString();
     } // end static method process(String, boolean)
     
+    /**
+     * this method returns the default table key in the specified case
+     * @param upperCase {@code boolean} determining the case of table key 
+     * @return table key of type {@code String}
+     * @author Michael Roth
+     * @since 2.2
+     */
     public static String getKey(boolean upperCase) {
         
         String key;
@@ -139,30 +190,45 @@ public final class KeyTable {
         return key;
     } // end method getKey(String, boolean)
     
-    public static String getKey(String key, boolean upperCase) {
+    /**
+     * this method is used to get the table key used in the encryption
+     * @param key a key of type {@code String} to be processed
+     * @param upperCase {@code boolean} value to determine the case of the table key
+     * @return table key of type {@code String}
+     * @author Michael Roth
+     * @since 2.2
+     */
+    public static String getKey(String k, boolean upperCase) {
         
-        ArrayList <Character> arr = new ArrayList();
-        if(upperCase) key = key.toUpperCase();
-        StringBuilder sB = new StringBuilder();
+        // ensures the key is in a right format
+        String key = KeyTable.process(k,false);
         
-        for(int i = 0; i<key.length();i++) {
-            char c = key.charAt(i);
-            if((c > 64 && c < 91) && !(arr.contains(c))) {
-                arr.add(c);
-                sB.append(c);
-            } // end if
-        } // end for
+        // instantiates a new StringBuilder
+        StringBuilder b = new StringBuilder();
         
-        if(arr.size() != 26) {
-            for(int i = 0; i < 26; i++) {
-                char c = (char) (i + 65);
-                if(!arr.contains(c)) {
-                    arr.add(c);
-                    sB.append(c);
-                } // end if()
-            } // end for
-        } // end if
+        // checks if the key variable is empty, if not, iterates the loop
+        while(!key.isEmpty()) {
+            
+            char c = key.charAt(0);
+            
+            // appends the first character in the key variable and deletes all other occurencs of that character
+            b.append(c);
+            key.replaceAll(Character.toString(c), "");
+            
+        } // end while()
         
-        return sB.toString();
+        // loops through the default key
+        for(int i = 0; i < DEFAULT_KEY.length(); i++) {
+            
+            // stores the current character in a variable and if it's not yet in the StringBuilder, appends it
+            char c = DEFAULT_KEY.charAt(i);
+            if(b.toString().contains(Character.toString(c))) {
+                b.append(c);
+            } // end if()
+        } // end for()
+        
+        // returns the String stored in the StringBuilder depending on the case
+        if(upperCase) return b.toString();
+        else return b.toString().toLowerCase();
     } // end method getKey(String, boolean)
 } // end class KeyTable

43a8a10ac1e049d22067ff350f677e0e5a9ba21e
Michael Roth <michael.roth@mensa.cz>
2017-05-18 10:29:42 +0200

    Added Dory
    an Easter egg, an exception used as an excuse for unimplemented parts of code

---
+++ Dory.java
@@ -0,0 +1,19 @@
+
+
+/**
+ * This exception is here to justificate all the possible errors which may occur in the {@code Vigenere} project,</br>
+ * meaning she is responsible for all the unsolvable problems. {@code Dory} shall be deemed to blame for any problems in the project
+ *@see <a href="https://www.facebook.com/iva.zahradnikova.10?fref=ts">Iva Zahradnikova</a>
+ *@since 12.4.1998
+ */
+public class Dory extends Exception
+{
+    public Dory() {
+        
+    } // end constructor {@code Dory()}
+
+    public Dory(String wrong_direction_inputted) {
+       
+    }
+    
+} // end {@code Dory}
